2f8c44f34d9e3d54508aebca9612e0be05fc48ba
Dominika Miodowska <dominika.miodowska@ongoing.ch>
2019-10-30 16:10:44 +0100

    add API key and GET route

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {
+    date: newDate,
+    temp:   temp,
+    content: content
+};
 
 // Require Express to run server and routes
 const express = require('express');
+
 // Start up an instance of app
 const app = express();
 const bodyParser = require('body-parser');
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-// const cors = require("cors");
-// app.use(cors());
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
@@ -29,15 +35,22 @@ function listening(){
     
 };
 
-// Initialize the main project folder
+// Initialize all route with a callback function
 
+// const data = [];
+const locationData = [];
+app.get('/all', getData)
+function getData(req, res){
+    res.send(locationData)
+}
 
-const data = [];
-
-app.post('/addMovie', function(req, res){
-   data.push(req.body); 
-   console.log(data);
+app.post('/', function(req, res){
+    newEntry={
+        location: req.body.location,
+        feeling: req.body.feeling
+    }
+   projectData.push(newEntry)
+   res.send.apply(locationData)
+   console.log(projectData);
    
 });
-
-// Initialize all route with a callback function
--- website/app.js
+++ website/app.js
@@ -1,29 +1,68 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const baseURL = 'api.openweathermap.org/data/2.5/weather?q=';
+// 'London,uk&APPID='
+const apiKey = '7d9ab1f237d14cf0daf5898c7036e76f';
+
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+  const newLocation = document.getElementById('zip').value;
+  getLocation(baseURL, newLocation, apiKey)
+}
+
+const getLocation = async(baseURL, location, key) =>{
+  const res = await fetch(baseURL+location+key)
+ try {
+  const data = await res.json();
+  console.log(data);
+  
+  } catch(error){
+  console.log("error", error);
+  }
+}
+
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getDate()+'.'+ d.getMonth()+'.'+ d.getFullYear();
+console.log(`Today is : ${newDate}`);
 
-const postData = async ( url = '', data = {})=>{
-    console.log(data);
-      const response = await fetch(url, {
-      method: 'POST', 
-      credentials: 'same-origin',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-     // Body data type must match "Content-Type" header        
-      body: JSON.stringify(data), 
-    });
-
-      try {
-        const newData = await response.json();
-        console.log(newData);
-        return newData;
-      }catch(error) {
-      console.log("error", error);
-      }
+// UI UPDATE
+const updateUI = async () => {
+  const request = await fetch('/all')
+  try{
+    const allData = await request.json()
+    console.log(allData);
+    document.getElementById('date').innerHTML = allData[0].date;
+    document.getElementById('temp').innerHTML = allData[0].temp;
+    document.getElementById('content').innerHTML = allData[0].feeling;
+  
+  }catch(error){
+    console.log("error", error);
   }
+}
+// const postData = async ( url = '', data = {})=>{
+//     console.log(data);
+//       const response = await fetch(url, {
+//       method: 'POST', 
+//       credentials: 'same-origin',
+//       headers: {
+//           'Content-Type': 'application/json',
+//       },
+//      // Body data type must match "Content-Type" header        
+//       body: JSON.stringify(data), 
+//     });
 
-postData('/addMovie', {movie:' the matrix', score:2});
-postData('/addMovie', {movie:' the scorpion', score:5.2});
+//       try {
+//         const newData = await response.json();
+//         console.log(newData);
+//         return newData;
+//       }catch(error) {
+//       console.log("error", error);
+//       }
+//   }
+
+//  postData('/addMovie', {movie:' the matrix', score: 5})
