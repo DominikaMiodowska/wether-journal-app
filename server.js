39cbbb46006d196c8b2fdcb4900fea78a34543f7
Dominika Miodowska <dominika.miodowska@ongoing.ch>
2019-11-04 15:19:19 +0100

    add API, GET, POST

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
+//jshint esversion: 6
+ 
 // Setup empty JS object to act as endpoint for all routes
-projectData = {
-    date: newDate,
-    temp:   temp,
-    content: content
-};
+projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
+const request = require('request');
 
 // Start up an instance of app
 const app = express();
@@ -24,7 +23,6 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
 const port = 8280;
 const server = app.listen(port, listening);
@@ -35,22 +33,37 @@ function listening(){
     
 };
 
-// Initialize all route with a callback function
 
-// const data = [];
+// const fakelocation = {
+//     zip : '94040,uk',
+//     temp: 50,
+//     content: 'London is very cold'
+// }
+// app.get('/fakeDataLocation', getFakeData)
+// function getFakeData(req, res){
+//     res.send(fakelocation)
+// }
+
 const locationData = [];
 app.get('/all', getData)
 function getData(req, res){
     res.send(locationData)
 }
 
-app.post('/', function(req, res){
+//POST route
+
+app.post('/add', function(req, res){   
+    // console.log(req.body)
+    
     newEntry={
-        location: req.body.location,
+        // date: req.body.date,
+        zip: req.body.zip,
+        temp: req.main.temp,
         feeling: req.body.feeling
     }
-   projectData.push(newEntry)
-   res.send.apply(locationData)
-   console.log(projectData);
+    locationData.push(newEntry)
+    res.send(locationData)
+    console.log(locationData);
+}
+ );
    
-});
--- website/app.js
+++ website/app.js
@@ -1,34 +1,67 @@
 /* Global Variables */
 // Personal API Key for OpenWeatherMap API
-const baseURL = 'api.openweathermap.org/data/2.5/weather?q=';
-// 'London,uk&APPID='
-const apiKey = '7d9ab1f237d14cf0daf5898c7036e76f';
+const apiKey = '&appid=7d9ab1f237d14cf0daf5898c7036e76f';
+const baseURL = `api.openweathermap.org/data/2.5/weather?zip=${zip}${apiKey}`;
+
 
 
 document.getElementById('generate').addEventListener('click', performAction);
 
 function performAction(e) {
-  const newLocation = document.getElementById('zip').value;
-  getLocation(baseURL, newLocation, apiKey)
-}
+  const zip = document.getElementById('zip').value;
+  const feeling = document.getElementById('feelings').value;
 
-const getLocation = async(baseURL, location, key) =>{
-  const res = await fetch(baseURL+location+key)
- try {
-  const data = await res.json();
+getLocation(baseURL, zip, apiKey)
+
+.then(function(data){
   console.log(data);
-  
-  } catch(error){
+  //Add data to post request  date: data.date,
+  postData('/add', { temp:data.temp, zip:zip, feeling:feeling})
+
+  // show in UI
+  updateUI()
+});
+};
+
+const getLocation = async(baseURL, zip, key) =>{
+
+  const res = await fetch(baseURL + zip + key);
+
+  try {
+    const data = await res.json();
+    console.log(data);
+} catch(error){
   console.log("error", error);
   }
 }
+const postData = async ( url = '', data = {})=>{
+    console.log(data);      
+      const response = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
 
 
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getDate()+'.'+ d.getMonth()+'.'+ d.getFullYear();
+let newDate = d.getDate()+'.'+ (d.getMonth()+1)+'.'+ d.getFullYear();
 console.log(`Today is : ${newDate}`);
+// const content = document.getElementById('date').innerHTML = `<h2>Today is ${newDate}</h2>`;
 
 // UI UPDATE
 const updateUI = async () => {
@@ -36,7 +69,8 @@ const updateUI = async () => {
   try{
     const allData = await request.json()
     console.log(allData);
-    document.getElementById('date').innerHTML = allData[0].date;
+    const content = document.getElementById('date').innerHTML = `<h2>${newDate}</h2>`;
+    // document.getElementById('date').innerHTML = allData[0].date;
     document.getElementById('temp').innerHTML = allData[0].temp;
     document.getElementById('content').innerHTML = allData[0].feeling;
   
@@ -44,25 +78,5 @@ const updateUI = async () => {
     console.log("error", error);
   }
 }
-// const postData = async ( url = '', data = {})=>{
-//     console.log(data);
-//       const response = await fetch(url, {
-//       method: 'POST', 
-//       credentials: 'same-origin',
-//       headers: {
-//           'Content-Type': 'application/json',
-//       },
-//      // Body data type must match "Content-Type" header        
-//       body: JSON.stringify(data), 
-//     });
 
-//       try {
-//         const newData = await response.json();
-//         console.log(newData);
-//         return newData;
-//       }catch(error) {
-//       console.log("error", error);
-//       }
-//   }
-
-//  postData('/addMovie', {movie:' the matrix', score: 5})
+postData('/add', {temp:'38', feeling: 'Today was cold.'})
